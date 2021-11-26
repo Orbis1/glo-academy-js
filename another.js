b386c1f24fae6ca7191c9164aee2c232ba86be93
Orbis <infectores@gmail.com>
2021-11-26 18:37:43 +0300

    simplification

--- another.js
+++ another.js
@@ -1,52 +1,5 @@
 const num = 266219;
-
-const runTask = ({ description, fn }) => {
-    console.log("//" + description);
-    result = fn(result);
-    console.log(result);
-    console.log("");
-};
-
-const tasks = [
-    {
-        description: "Вывести в консоль произведение (умножение) цифр этого числа",
-        fn: () => {
-            let result = 1;
-            const str = num.toString();
-            const arr = str.split('');
-
-            console.log(arr.join(", "));
-
-            arr.forEach(n => {
-                console.log(`${result} * ${n} = ${result * n}`);
-                result *= n;
-            });
-
-            return result;
-        },
-    },
-    {
-        description: "Вывести в консоль произведение (умножение) цифр этого числа (через eval)",
-        fn: () => {
-            const str = num.toString();
-            const arr = str.split('');
-            const str4eval = arr.join('*')
-            return eval(str4eval);
-        },
-    },
-    {
-        description: "Полученный результат возвести в степень 3, используя только 1 оператор (Math.pow не подходит)",
-        fn: (prevResult) => {
-            return prevResult ** 3;
-        },
-    },
-    {
-        description: "Вывести в консоль первые 2 цифры полученного числа",
-        fn: (prevResult) => {
-            return prevResult.toString().slice(0,2);
-        },
-    },
-];
-
-let result;
-tasks.forEach(task => runTask(task));
+const res = num.toString().split('').reduce((acc, n) => acc * n)
+console.log(res);
+console.log(res ** 3);
+console.log(res.toString().slice(0,2));
--- script.js
+++ script.js
@@ -5,46 +5,11 @@ const rollback = 49;
 const fullPrice = 100000;
 const adaptive = true;
 
-const runTask = ({description, fn}) => {
-    console.log('//' + description);
-    fn();
-    console.log('');
-}
-
-const tasks = [
-    {
-        description: 'Вывести в консоль тип данных значений переменных title, fullPrice, adaptive',
-        fn: () => {
-            console.log(typeof title);
-            console.log(typeof fullPrice);
-            console.log(typeof adaptive);
-        }
-    },
-    {
-        description: 'Вывести в консоль длину строки screens',
-        fn: () => {
-            console.log(screens.length)
-        }
-    },
-    {
-        description: 'Вывести в консоль “Стоимость верстки экранов...',
-        fn: () => {
-            console.log(`“Стоимость верстки экранов ${screenPrice} рублей/ долларов/гривен/юани” 
-            и “Стоимость разработки сайта ${fullPrice} рублей/ долларов/гривен/юани”`);
-        }
-    },
-    {
-        description: 'Привести строку screens к нижнему регистру и разбить строку на массив, вывести массив в консоль...',
-        fn: () => {
-            console.log(screens.toLowerCase().split(', '))
-        }
-    },
-    {
-        description: 'Вывести в консоль Процент отката посреднику за работу (fullPrice * (rollback/100))',
-        fn: () => {
-            console.log(fullPrice * (rollback/100))
-        }
-    }
-]
-
-tasks.forEach(task => runTask(task));
+console.log(typeof title);
+console.log(typeof fullPrice);
+console.log(typeof adaptive);
+console.log(screens.length)
+console.log(`“Стоимость верстки экранов ${screenPrice} рублей/ долларов/гривен/юани” 
+и “Стоимость разработки сайта ${fullPrice} рублей/ долларов/гривен/юани”`);
+console.log(screens.toLowerCase().split(', '))
+console.log(fullPrice * (rollback/100))
